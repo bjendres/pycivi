a783e58bc0c9858e2a3d9a968fb58e919a658671
systopia <endres@systopia.de>
2013-11-08 15:18:22 +0100

    added GPLv3

--- pycivi/importer.py
+++ pycivi/importer.py
@@ -1,3 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+This is a python API wrapper for CiviCRM (https://civicrm.org/)
+Copyright (C) 2013 Systopia  (endres@systopia.de)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+__author__      = "Björn Endres"
+__copyright__   = "Copyright 2013, Systopia"
+__license__     = "GPLv3"
+__maintainer__  = "Björn Endres"
+__email__       = "endres[at]systopia.de"
+
+
 import CiviCRM, entity_type
 import csv
 import codecs
