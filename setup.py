df7022b597177b95b0924150d3a3a09392e59e36
Thomas Leichtfuß <leichtfuss@systopia.de>
2021-11-18 10:42:45 +0100

    [setup] added chardet and charset_normalizer as dependecies
    These libs are dependencies from requests. But requests doesn't seem to be reliable in listing these dependencies.

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup
 setup(
     name = 'pycivi',
     packages = ['pycivi'],
-    install_requires = ['requests'],
+    install_requires = [
+        'requests',
+        'chardet',
+        'charset_normalizer',
+    ],
 )
